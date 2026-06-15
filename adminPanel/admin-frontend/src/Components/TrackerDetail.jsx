a0a887798796ade12e8b8c1898c6b379859a8036
SSK166 <sreekrishnassk166@gmail.com>
2026-06-15 12:59:32 +0530

    Bugs fixed for admin panel detailed summary

--- adminPanel/admin-frontend/src/Components/TrackerDetail.jsx
+++ adminPanel/admin-frontend/src/Components/TrackerDetail.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useParams, NavLink } from "react-router-dom";
 import "../../../static/style.css"; 
 
-
 export default function TrackerDetail() {
     const { trackerId } = useParams(); 
 
