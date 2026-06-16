6ac9dbf2006794a8087bdc2664150bb9b7caed41
SSK166 <sreekrishnassk166@gmail.com>
2026-06-16 17:25:18 +0530

    DB Name modification error fixed for deployment in Project Tracker

--- ProjectTracker/projectDatabase.py
+++ ProjectTracker/projectDatabase.py
@@ -28,7 +28,7 @@ def get_conn():
     conn = psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
-        dbname=os.getenv("DB_NAME"),
+        dbname=os.getenv("PROJECT_DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         sslmode= "require" if os.getenv('ENVIRONMENT') else "disable"
